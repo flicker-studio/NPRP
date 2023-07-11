36c52dc240de2983cd3be5a54586db82d41789ee
AvA <Morsiusiurandum@outlook.com>
2023-07-11 22:27:19 +0800

    refactor: add .editorconfig file and update name of the NativeParallelMultiHashMap

---
+++ Assets/Editor/SmoothNormals/CollectNormalJob.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+internal struct CollectNormalJob : IJobParallelFor
+{
+    // Mark read-only to improve performance
+    [ReadOnly] private NativeArray<Vector3> _normals, _vertex;
+
+    private NativeParallelMultiHashMap<Vector3, Vector3>.ParallelWriter _result;
+
+    public CollectNormalJob(NativeArray<Vector3> normals, NativeArray<Vector3> vertex,
+        NativeParallelMultiHashMap<Vector3, Vector3>.ParallelWriter result)
+    {
+        _normals = normals;
+        _vertex = vertex;
+        _result = result;
+    }
+
+    void IJobParallelFor.Execute(int index)
+    {
+        _result.Add(_vertex[index], _normals[index]);
+    }
+}
