56a03e6a92afec799d1ef86960ddd42ec3f02249
AvA <Morsiusiurandum@outlook.com>
2023-07-14 23:07:56 +0800

    docs: add PackageVersion annotation

--- Assets/Editor/PackageVersion.cs
+++ Assets/Editor/PackageVersion.cs
@@ -4,37 +4,41 @@ using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
-public class PackageVersion
+public static class PackageVersion
 {
     private static ListRequest _request;
-    private static int ProgressId = Progress.Start("Package version check");
+    private static int _progressId;
 
-    [MenuItem("Window/Package Version Check Example")]
+    /// <summary>
+    /// Package check will start on Editor start
+    /// </summary>
+    [InitializeOnLoadMethod]
     public static void GetPackageList()
     {
         // List packages installed for the project
         _request = Client.List();
-
+        _progressId = Progress.Start("Package version check");
         //Injection update loop
         EditorApplication.update += VersionCheck;
     }
 
+    /// <summary>
+    /// Make sure the collection version conforms to the specification
+    /// </summary>
+    /// <exception cref="ArgumentOutOfRangeException">Error in Status</exception>
     private static void VersionCheck()
     {
         switch (_request.Status)
         {
             case StatusCode.Success:
             {
-                Progress.Report(ProgressId, 2, 3, "Checking the version");
+                Progress.Report(_progressId, 2, 3, "Checking the version");
                 //version detection operation
                 foreach (var info in _request.Result)
-                {
                     if (info.name == "com.unity.collections")
-                    {
                         Debug.Log(info.version);
-                    }
-                }
-                Progress.Report(ProgressId, 3, 3);
+
+                Progress.Report(_progressId, 3, 3);
                 break;
             }
             case StatusCode.Failure:
@@ -44,14 +48,14 @@ public class PackageVersion
             }
             case StatusCode.InProgress:
             {
-                Progress.Report(ProgressId, 1, 3, "Waiting package list");
+                Progress.Report(_progressId, 1, 3, "Waiting package list");
                 return;
             }
             default:
                 throw new ArgumentOutOfRangeException();
         }
 
-        Progress.Remove(ProgressId);
+        Progress.Remove(_progressId);
         EditorApplication.update -= VersionCheck;
     }
 }
